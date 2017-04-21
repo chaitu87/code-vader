444f17a1203dcff08da95e297fd1ffda3b0e69a5
chaithanya padi <chaithanya@theinnerhour.com>
2017-04-21 21:56:41 +0530

    active styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,17 @@
-var app = angular.module("code-vader", [])
+var app = angular.module("code-vader", ['ui.codemirror'])
     .controller('masterCtrl', function($scope) {
         $scope.firstname = "Chaithanya";
         $scope.lastname = "Padi";
+        $scope.editorOptions = {
+            lineWrapping: true,
+            lineNumbers: true,
+            mode: 'javascript',
+            scrollbarStyle: null
+        };
         $scope.snippets = [{
             title: 'Welcome Code',
             desc: 'this code is a welcome code ... we test highlight here',
-            code: 'var wow="hello";console.log(wow);',
+            code: 'var wow="hello";\nconsole.log(wow);',
             language: 'javascript'
         }, {
             title: 'Other Test',
@@ -13,4 +19,9 @@ var app = angular.module("code-vader", [])
             code: 'puts "hello"',
             language: 'ruby'
         }];
+        $scope.activeCode = $scope.snippets[0].code;
+        $scope.openSnippet = function(snippet){
+        	console.log(snippet);
+        	$scope.activeCode = snippet.code;
+        };
     });
