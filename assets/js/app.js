23bb6b82f189c0f0c1e7d23bc5068e995f99d7fc
chaithanya padi <chaithanya@theinnerhour.com>
2017-04-21 21:32:34 +0530

    added codemirror dependency

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,15 @@ var app = angular.module("code-vader", [])
     .controller('masterCtrl', function($scope) {
         $scope.firstname = "Chaithanya";
         $scope.lastname = "Padi";
+        $scope.snippets = [{
+            title: 'Welcome Code',
+            desc: 'this code is a welcome code ... we test highlight here',
+            code: 'var wow="hello";console.log(wow);',
+            language: 'javascript'
+        }, {
+            title: 'Other Test',
+            desc: 'Test different languages',
+            code: 'puts "hello"',
+            language: 'ruby'
+        }];
     });
